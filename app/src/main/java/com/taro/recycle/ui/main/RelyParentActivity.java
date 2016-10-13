17a2c175490b6b7fe070617a9e615686398f9d9f
henry <henry@bestapp.us>
2016-10-14 02:29:25 +0800

    更新依赖界面的示例

--- app/src/main/java/com/taro/recycle/ui/main/RelyParentActivity.java
+++ app/src/main/java/com/taro/recycle/ui/main/RelyParentActivity.java
@@ -17,6 +17,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.taro.headerrecycle.adapter.AdjustCountAdatperOption;
 import com.taro.headerrecycle.adapter.HeaderRecycleAdapter;
 import com.taro.headerrecycle.adapter.HeaderRecycleViewHolder;
 import com.taro.headerrecycle.adapter.SimpleRecycleAdapter;
@@ -36,11 +37,13 @@ public class RelyParentActivity extends AppCompatActivity implements View.OnClic
     Button mBtnItemCount;
     EditText mEtHeight;
     Button mBtnHeight;
+    Button mBtnAutoCount;
+    Button mBtnAutoHeight;
     RecyclerView mRvRely;
 
     private int mMaxCount;
     private int mTempCount;
-    private RelyAdapterOption mOption;
+    private AdjustCountAdatperOption mAdjustOption;
     private SimpleRecycleAdapter mAdapter;
 
     @Override
@@ -53,20 +56,26 @@ public class RelyParentActivity extends AppCompatActivity implements View.OnClic
         mBtnItemCount = (Button) findViewById(R.id.btn_rely_confirm);
         mEtHeight = (EditText) findViewById(R.id.et_rely_rv_height);
         mBtnHeight = (Button) findViewById(R.id.btn_rely_update_height);
+        mBtnAutoCount = (Button) findViewById(R.id.btn_rely_auto_update_5);
+        mBtnAutoHeight = (Button) findViewById(R.id.btn_rely_auto_update_40);
         mRvRely = (RecyclerView) findViewById(R.id.rv_rely);
 
         List<String> list = new ArrayList<String>(100);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             list.add("");
         }
 
-        mOption = new RelyAdapterOption();
-        mAdapter = new SimpleRecycleAdapter<String>(this, mOption, list);
+        mAdjustOption = new RelyAdapterOption2();
+        //设置为在bindView时进行计算;否则notifyDataSetChanged不一定会更新界面
+        mAdjustOption.setComputeWhen(AdjustCountAdatperOption.COMPUTE_WHEN_BIND_VIEW);
+        mAdapter = new SimpleRecycleAdapter<String>(this, mAdjustOption, list);
 
         mRvRely.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         mRvRely.setAdapter(mAdapter);
         mBtnItemCount.setOnClickListener(this);
         mBtnHeight.setOnClickListener(this);
+        mBtnAutoCount.setOnClickListener(this);
+        mBtnAutoHeight.setOnClickListener(this);
     }
 
     @Override
@@ -84,9 +93,9 @@ public class RelyParentActivity extends AppCompatActivity implements View.OnClic
                     Toast.makeText(this, "小于0使用最大item数", Toast.LENGTH_SHORT).show();
                 }
                 //TODO:一定要先设置这个方法,否则更新recycleView时会因为缓存的view数量问题报错的.
-                mOption.setAdjustCount(mTempCount);
-                mOption.setIsRecompute(true);
-                mRvRely.setAdapter(mAdapter);
+                mAdjustOption.setAdjustCount(mTempCount);
+                mAdjustOption.setIsAutoCompute(false);
+                mAdapter.notifyDataSetChanged();
                 break;
             case R.id.btn_rely_update_height:
                 CharSequence height = mEtHeight.getText();
@@ -101,14 +110,50 @@ public class RelyParentActivity extends AppCompatActivity implements View.OnClic
                             return;
                         }
                         RecyclerViewUtil.setViewWidthAndHeight(mRvRely, ViewGroup.LayoutParams.MATCH_PARENT, (int) dpx);
-                        mOption.resetRvHeight();
-                        mRvRely.setAdapter(mAdapter);
+                        mAdjustOption.setForceRecompute();
+                        mAdjustOption.setIsAutoCompute(true);
+                        mAdapter.notifyDataSetChanged();
                     }
                 }
+                break;
+            case R.id.btn_rely_auto_update_5:
+                mAdjustOption.setAdjustCount(5);
+                mAdjustOption.setIsAutoCompute(false);
+                mAdapter.notifyDataSetChanged();
+                break;
+            case R.id.btn_rely_auto_update_40:
+                float dpx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, this.getResources().getDisplayMetrics());
+                RecyclerViewUtil.setViewWidthAndHeight(mRvRely, ViewGroup.LayoutParams.MATCH_PARENT, (int) dpx);
+                mAdjustOption.setForceRecompute();
+                mAdjustOption.setIsAutoCompute(true);
+                mAdapter.notifyDataSetChanged();
                 break;
         }
     }
 
+    private class RelyAdapterOption2 extends AdjustCountAdatperOption<String> {
+        @Override
+        public int getViewType(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getLayoutId(int viewType) {
+            return R.layout.item_rely;
+        }
+
+        @Override
+        public void setViewHolder(String itemData, int position, @NonNull HeaderRecycleViewHolder holder) {
+            super.setViewHolder(itemData, position, holder);
+
+            ImageView itemView = (ImageView) holder.getRootView();
+            itemView.setImageResource(R.drawable.bg);
+            itemView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+    }
+
+    //历史测试使用的文件,不再使用
+    @Deprecated
     private class RelyAdapterOption extends SimpleRecycleAdapter.SimpleAdapterOption<String> {
         private boolean mIsRecompute = true;
         private Point mParentParams;
@@ -147,26 +192,9 @@ public class RelyParentActivity extends AppCompatActivity implements View.OnClic
 
         @Override
         public void setViewHolder(String itemData, int position, HeaderRecycleViewHolder holder) {
-            ImageView itemView = (ImageView) holder.getRootView();
-            itemView.setImageResource(R.drawable.bg);
-            itemView.setScaleType(ImageView.ScaleType.FIT_XY);
-        }
-
-        @Override
-        public int getLayoutId(int viewType) {
-            return R.layout.item_rely;
-        }
-
-        @Override
-        public void onCreateViewEverytime(@NonNull View itemView, @NonNull ViewGroup parentView, @NonNull HeaderRecycleAdapter adapter, int viewType) {
-            //每一次view被创建时都会回调此方法,这里是进行view宽高设置的好地方;
-            //但是需要注意根据需求来确定当前的计算和设置的代码应该放在哪里;
-            //setViewHolder是每一次绑定数据时都会进行调用的地方;而onCreate则只有创建时会调用;
-            //如果创建缓存的view足够多时,在动态更新item数量时onCreate并不会被调用,而setViewHolder是一定会被调用的;
-            //所以应该根据需求确定界面相关的更新代码应该放在何处会更好.
             if (mParentParams.equals(0, 0)) {
                 //计算recycleView界面的宽高
-                RecyclerViewUtil.computeParentViewDrawArea(parentView, mParentParams);
+                RecyclerViewUtil.computeParentViewDrawArea(holder.getAdatper().getParentRecycleView(), mParentParams);
             }
             if (mMaxCount <= 0) {
                 //计算基于宽/高可填充的item数量
@@ -184,7 +212,47 @@ public class RelyParentActivity extends AppCompatActivity implements View.OnClic
                 this.setAdjustCount(mTempCount);
             }
             //设置每一个创建itemView的参数,宽高/margin/额外的margin部分
-            RecyclerViewUtil.computeAndSetSquareViewLayoutParams(itemView, 0, 0, 0, 0, mChildParams.y, mParentParams.x, mParentParams.y, false);
+            RecyclerViewUtil.computeAndSetSquareViewLayoutParams(holder.getRootView(), 0, 0, 0, 0, mChildParams.y, mParentParams.x, mParentParams.y, false);
+
+
+            ImageView itemView = (ImageView) holder.getRootView();
+            itemView.setImageResource(R.drawable.bg);
+            itemView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
+        @Override
+        public int getLayoutId(int viewType) {
+            return R.layout.item_rely;
+        }
+
+        @Override
+        public void onCreateViewEverytime(@NonNull View itemView, @NonNull ViewGroup parentView, @NonNull HeaderRecycleAdapter adapter, int viewType) {
+            //每一次view被创建时都会回调此方法,这里是进行view宽高设置的好地方;
+            //但是需要注意根据需求来确定当前的计算和设置的代码应该放在哪里;
+            //setViewHolder是每一次绑定数据时都会进行调用的地方;而onCreate则只有创建时会调用;
+            //如果创建缓存的view足够多时,在动态更新item数量时onCreate并不会被调用,而setViewHolder是一定会被调用的;
+            //所以应该根据需求确定界面相关的更新代码应该放在何处会更好.
+//            if (mParentParams.equals(0, 0)) {
+//                //计算recycleView界面的宽高
+//                RecyclerViewUtil.computeParentViewDrawArea(parentView, mParentParams);
+//            }
+//            if (mMaxCount <= 0) {
+//                //计算基于宽/高可填充的item数量
+//                mMaxCount = RecyclerViewUtil.computeSquareViewCountOnParentView(mParentParams.x, mParentParams.y, false);
+//            }
+//            //若当前的指定item数量不合法,使用可填充的最大item数量代替
+//            if (mTempCount < 0 || mTempCount > mMaxCount) {
+//                mTempCount = mMaxCount;
+//            }
+//            //根据标志或数据判断是否需要重新计算一次界面item之前的边距值(即额外的margin部分)
+//            if (mIsRecompute || mChildParams.y == Integer.MIN_VALUE) {
+//                mChildParams.y = RecyclerViewUtil.computeSquareViewEdgeSize(mParentParams.x, mParentParams.y, mTempCount, false);
+//                mIsRecompute = false;
+//                //更新调整的界面item数量
+//                this.setAdjustCount(mTempCount);
+//            }
+//            //设置每一个创建itemView的参数,宽高/margin/额外的margin部分
+//            RecyclerViewUtil.computeAndSetSquareViewLayoutParams(itemView, 0, 0, 0, 0, mChildParams.y, mParentParams.x, mParentParams.y, false);
         }
     }
 }
